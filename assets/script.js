f52331d2452fb6148ce9ff0e882622576a8f3ce3
Gabe Vee <0gabevee0@gmail.com>
2022-08-06 15:54:33 -0600

    completed confirm pop up logic to add all types of characters into password

--- assets/script.js
+++ assets/script.js
@@ -21,22 +21,6 @@ function generatePassword() {
   var num = "0123456789";
   var special = "+-&|!(){}[]^~*?:=_#@<>/,.;";
 
-  //Split variables
-  var lSplit = lowLetters.split("");
-  var uSplit = upLetters.split("");
-  var nSplit = num.split("");
-  var speSplit = special.split("");
-  var allSplit1 = lowLetters.concat(upLetters, num, special).split("");
-
-  var allSplit2 = {
-    low: lSplit,
-    up: uSplit,
-    number: nSplit,
-    spec: speSplit,
-  };
-
-  var allSplit3 = [lSplit, uSplit, nSplit, speSplit];
-
   //prompt user for length
   var Length = prompt("PassWord needs to be more than 8 and less than 128:");
 
@@ -47,25 +31,26 @@ function generatePassword() {
     alert("Restarting Webpage Password is too long");
     window.location.reload();
   } else {
-    //prompt user to confirm
-    var promptArray = [
-      (lowCase = confirm("do you want Lower case letters")),
-      (upperCase = confirm("do you want capital Letters")),
-      (specialChar = confirm("do you want special characters")),
-      (numbers = confirm("do you want numbers")),
-    ];
+    //prompt user
+    var lowCase = confirm("do you want Lower case letters");
+    var upperCase = confirm("do you want capital Letters");
+    var specialChar = confirm("do you want special characters");
+    var numbers = confirm("do you want numbers");
+    var passwordOptionsTxt = "";
 
-    // iterate through each prompt
-    for (var i = 0; i < promptArray.length; i++) {
-      if (promptArray[i] === true) {
-      }
-    }
+    lowCase === true ? (passwordOptionsTxt += lowLetters) : "";
+    upperCase === true ? (passwordOptionsTxt += upLetters) : "";
+    specialChar === true ? (passwordOptionsTxt += special) : "";
+    numbers === true ? (passwordOptionsTxt += num) : "";
+    var passhold = passwordOptionsTxt.split("");
+
+    console.log(passhold);
   }
 
   //generate the password using user input
   var randomPassword = "";
-  for (var i = 0, p = lSplit.length; i < Length; i++) {
-    randomPassword += lSplit[Math.floor(Math.random() * p)];
+  for (var i = 0, p = passhold.length; i < Length; i++) {
+    randomPassword += passhold[Math.floor(Math.random() * p)];
   }
 
   console.log(randomPassword);
